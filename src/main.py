8624221cfbb0dbcdad778da20da2774d4cfe729b
Serhii Smirnov <snoff87@gmail.com>
2024-09-09 19:19:07 +0300

    some progress

--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
 from schemas import State
 from cube import Cube
+from typing import Optional
 app = FastAPI()
 
-@app.get("/")
-def turn_face(state: State, direction: str):
-    newCube = Cube()
+@app.get("/show")
+def show_face(state: Optional[State], side: str):
+    newCube = Cube({"Top": state.top, "Bottom": state.bottom, "Left": state.left,
+                    "Right": state.right, "Front": state.front, "Back": state.back})
+    return newCube.print_cube(side)
--- src/schemas.py
+++ src/schemas.py
@@ -2,7 +2,6 @@ from pydantic import BaseModel
 from typing import List
 
 class State(BaseModel):
-    face: str
     top: List
     bottom: List
     left: List
