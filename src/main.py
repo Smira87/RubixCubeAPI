3b1a4d28a6eda6a03132c0c5a702f230801f1ba9
Serhii Smirnov <snoff87@gmail.com>
2024-09-09 19:27:12 +0300

    some progress 2

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from typing import Optional
 app = FastAPI()
 
 @app.get("/show")
-def show_face(state: Optional[State], side: str):
+def show_face(state: State, side: Optional[str] = "Front"):
     newCube = Cube({"Top": state.top, "Bottom": state.bottom, "Left": state.left,
                     "Right": state.right, "Front": state.front, "Back": state.back})
     return newCube.print_cube(side)
