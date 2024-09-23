832fe8b66f3e2f5f21c98b6d2e16be145b09b18d
Serhii Smirnov <snoff87@gmail.com>
2024-09-23 20:24:25 +0300

    turn_right done, pydentic schema correction started

--- src/operations/router.py
+++ src/operations/router.py
@@ -9,6 +9,11 @@ router = APIRouter(
 )
 @router.post("/")
 def show_face(state: Optional[State] = None, side: Optional[str] = "front"):
-    newCube = Cube({"top": state.top, "bottom": state.bottom, "left": state.left,
-                    "right": state.right, "front": state.front, "back": state.back})
+    newCube = Cube(state)
     return newCube.print_cube(side)
+
+@router.post("/turn")
+def turn_cube(direction: Optional[str] = "right", times: Optional[int] = 1, state: Optional[State] = None):
+    newCube = Cube(state)
+    newCube.turn_cube(direction, times)
+    return newCube
--- src/schemas.py
+++ src/schemas.py
@@ -2,9 +2,9 @@ from pydantic import BaseModel
 from typing import List
 
 class State(BaseModel):
-    top: List
-    bottom: List
-    left: List
-    right: List
-    front: List
-    back: List
+    top: List[List[List[int]]]
+    bottom: List[List]
+    left: List[List]
+    right: List[List]
+    front: List[List]
+    back: List[List]
